8610de385e11cc56957a6590ef516a36401e848a
ppalee1217 <ppalee1217@gmail.com>
2023-08-05 17:37:56 +0800

    Report ver.

--- config.h
+++ config.h
@@ -4,12 +4,34 @@
 // typedef
 typedef unsigned long long addr_t;
 // Mapping mode
-#define ADDRESSING_MODE 0
+// void setAddrMappingMode(int mode){
+//   #define ADDRESSING_MODE mode
+// }
+#define ADDRESSING_MODE 1
 // Configurable parameters
-#define BANK_SIZE 4
+#define MISS_CYCLE 20
 #define REQ_QUEUE_SIZE 4
 #define MSHR_ENTRY 8
 #define MAF_ENTRY 4
-#define MISS_CYCLE 20
+#define BANK_SIZE 4
+// void setReqQEntryNum(int num){
+//   #define REQ_QUEUE_SIZE num
+// }
+
+// void setMSHREntryNum(int num){
+//   #define MSHR_ENTRY num
+// }
+
+// void setMAFEntryNum(int num){
+//   #define MAF_ENTRY num
+// }
+
+// void setCacheBankNum(int num){
+//   #define BANK_SIZE num
+// }
+
+// Get data from Cache
+int getTotalCycle();
+int getBankId();
 
 #endif
